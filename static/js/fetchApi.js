2e11e58e05f335a10dadf3d45632c20876fa53ba
tuananhcx2000 <tuananhcx2000@gmail.com>
2021-04-28 07:31:41 +0700

    this is my program use API

---
+++ static/js/fetchApi.js
@@ -0,0 +1,8 @@
+// sử lý bất đồng bộ thay thế cho promises
+// thông thường thì khi dùng callback return về promise phải đợi máy chỉ phản hồi lại nhưng dùng await sẽ đợi đến khi máy chủ
+// phản hồi lại thì mới chạy tiếp(nghĩa là kết thúc) và trả về kết quả
+//Fetch trả về một promise, promise sẽ được resolves, giá trị trả về sẽ được gán vào response
+fetchRequest = async (url) => {
+    const response = await fetch(url)
+    return response.json()
+}
